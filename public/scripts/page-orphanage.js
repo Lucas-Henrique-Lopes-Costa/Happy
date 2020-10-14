3b2de603aa4c7162d35acc3b55afa3caee12431b
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-14 13:41:01 -0300

    Fim da AULA

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,72 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickCoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+const map = L.map('mapid', options).setView([-20.1480625,-44.8850062], 16);
+
+// create and add title
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    // Pegando do Streat Map
+    // {
+    // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }
+).addTo(map);
+
+// create icon
+
+const icon = L.icon ({
+    iconUrl: "./public/images/map-marker.svg", 
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    iconAnchor: [170, 2]
+})
+
+
+// Create and add marker | Adicionar um pop-up
+L.
+marker([-20.1480625,-44.8850062], { icon })// marcação | procura a variável
+.addTo(map) // adiciona ao mapa
+
+// Image Gallary
+
+function selectImage(event) { //quando o button é clicado ele realiza as ações
+    const button = event.currentTarget
+    
+    // console.log('Cliquei')
+
+    // REMOVENDO TODOAS AS CLASSE ".active"
+        const buttons = document.querySelectorAll(".images button")
+
+        // console.log(buttons) //ver o que está dentro do array
+
+        // fazendo a função de remover
+
+            // FORMA CURTA
+            // buttons.forEach((button) => {
+            //     button.classList.remove("active")
+            // })
+            
+            // ou    
+            
+            buttons.forEach(removeActiveClass) //para cada botão faça essa funcionalidade
+
+            function removeActiveClass (button) {
+                button.classList.remove("active")
+            }
+
+    // SELECIONAR A IMAGEMM CLICADA
+        const image = button.children[0]
+        const imageContainer = document.querySelector('.orphanage-details > img') // pegando o Container
+
+    // ATUALIZAR O CANTEINER DE IMAGEM (IMAGEM GRANDE)
+        imageContainer.src = image.src // mudando o src do Container
+
+    // ADICIONAR A CLASSE ".active" PARA O BOTÃO CLICADO
+        button.classList.add('active')
+        
+}
---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,84 @@
+/* Mexendo no Console
+    // { color: "red" } // isto é um objeto
+    // camiseta = { color: "red" } // colocando na variável
+
+    // camiseta == "verde" //=> verdadeiro
+
+    // // acessando propriendades:
+
+    // camuseta.color == "verde" // => verdadeiro
+
+    // // acessando o HTML
+
+    // document.body.style.backgroundColor = "blue"
+
+    // // dando funcionalidades
+
+    // camiseta.suja = false 
+    // //ou
+    // camiseta.suja = true
+
+    // camiseta.verificarLimpeza = function () {
+    //     if(camiseta.suja) {
+    //         // se a camiseta estiver suja
+    //         return "camiseta suja"
+    //     } else {
+    //         return "camiseta limpa"
+    //     }
+    // }
+
+    // camiseta.verificarLimpeza()
+
+    // CRIANDO O MAPA
+
+    // Tipos de dados:
+    // String ""
+    // Number 01
+    // Object {}
+    // Boolean True ou False
+    // Array []
+*/
+
+// Options
+const options = {
+    touchZoom: false,
+    doubleClickCoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid', options).setView([-20.1480625,-44.8850062], 15);
+
+// create and add title
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    // Pegando do Streat Map
+    // {
+    // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }
+).addTo(map);
+
+// create icon
+
+const icon = L.icon ({
+    iconUrl: "./public/images/map-marker.svg", 
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    iconAnchor: [170, 2]
+})
+
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+// Create and add marker | Adicionar um pop-up
+L.
+marker([-20.1480625,-44.8850062], { icon })// marcação | procura a variável
+.addTo(map) // adiciona ao mapa
+.bindPopup(popup) //ligue o pop-up pegando as pré-definições
+// .openPopup(); // feche o pop-up

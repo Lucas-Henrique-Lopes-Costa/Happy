bc2a354c009ae357ecd907dc8332928f457f0d03
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-15 19:37:59 -0300

    Adicionando a Pasta

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -55,14 +55,15 @@ L.tileLayer(
 
 const icon = L.icon ({
     iconUrl: "./public/images/map-marker.svg", 
-    iconSize: [58,68],
-    iconAnchor: [29, 68]
+    iconSize: [58, 68],
+    iconAnchor: [29, 68], // fiza o icone
+    popupAnchor: [170, 2] // fixa o popup
 })
 
 // create popup overlay
 const popup = L.popup({
     closeButton: false,
-    className: 'map-popup',
+    className: "map-popup",
     minWidth: 240,
     minHeight: 240
 }).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
